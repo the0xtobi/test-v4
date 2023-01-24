25c6d2f995e7ab83509bc50cff764b4c8d52b861
blur <thetobiterra@gmail.com>
2023-01-24 14:19:56 +0100

    build

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const searchImages = () =>{
-axios.get('https://api.unsplash.com/search/photos', {
-    headers: {
-        Authorization: 'Client-ID w6Hn0sN1gyV2EWGt2ktBLJ0w_pjVJAgLPuk9hMqMBBs'
-    },
-    params: {
-        query: 'cars'
-    }
-})
+const searchImages = async () => {
+    await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID w6Hn0sN1gyV2EWGt2ktBLJ0w_pjVJAgLPuk9hMqMBBs'
+        },
+        params: {
+            query: 'cars'
+        }
+    })
 }

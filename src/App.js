0ed1edb098e6af0c84da5c18d6d36bf55932f502
blur <thetobiterra@gmail.com>
2023-01-26 09:45:45 +0100

    build

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import searchImages from './api'
 
 function App() {
 
-  const handleSubmit = (term) => {
-    searchImages(term)
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term)
   }
 
   return (
